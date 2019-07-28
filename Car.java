81064e200ec6c8ea34a33714207935dce6738cf9
ynf2039 <50137295+ynf2039@users.noreply.github.com>
2019-07-28 20:59:00 +0800

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * 总抽象类
+ */
+public abstract class Car {
+   public String name = "";
+   public int patronage = 0;//载客量
+   public double burden = 0;//载货量
+   public int Serial_Number = 0;
+   public int price = 0;
+}
